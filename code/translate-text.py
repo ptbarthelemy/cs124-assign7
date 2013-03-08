77e510666fd3cf7f7c34fc6e1d07a0b274bd2fbd
P. T. Barthelemy <p.t.barthelemy+git@gmail.com>
2013-03-08 14:08:40 -0800

    Will rerun model.

--- code/translate-text.py
+++ code/translate-text.py
@@ -34,7 +34,7 @@ class DirectTranslation:
 		self.source = []
 		f = open(filename)
 		for lineF in f.readlines():
-			self.source.append(findall(r"[^\., '\n]+'?", lineF.lower()))
+			self.source.append(findall(r"([^\., '\n]+'?|[,.])", lineF.lower()))
 		f.close()
 
 	def translateText(self):
@@ -42,7 +42,10 @@ class DirectTranslation:
 		for lineF in self.source:
 			lineE = []
 			for word in lineF:
-				lineE.extend(self.dictionary[word].split())
+				if word in ".,":
+					lineE.append(word)
+				else:
+					lineE.extend(self.dictionary[word].split())
 			self.target.append(lineE)
 
 	def posTagging(self):
@@ -61,59 +64,60 @@ class DirectTranslation:
 			# print "TARGET1 :", target
 			for i in range(len(target)):
 
-				# # RULE 1: NN JJ --> JJ NN
-				# if i < len(target) - 1:
-				# 	if target[i][1][:2] == 'NN' and target[i+1][1] == 'JJ':
-				# 		print "  switching", target[i][0], target[i+1][0]
-				# 		target.insert(i,target.pop(i+1))
+				# RULE 1: NN JJ --> JJ NN
+				if i < len(target) - 1:
+					if target[i][1][:2] == 'NN' and target[i+1][1] == 'JJ':
+						print "  switching", target[i][0], target[i+1][0]
+						target.insert(i,target.pop(i+1))
 
-				# # RULE 2: NN VBG --> VBG NN  
-				# if i < len(target) - 1:
-				# 	if target[i][1][:2] == 'NN' and target[i+1][1] == 'VBG':
-				# 		print "  switching", target[i][0], target[i+1][0]
-				# 		target.insert(i,target.pop(i+1))
+				# RULE 2: NN VBG --> VBG NN  
+				if i < len(target) - 1:
+					if target[i][1][:2] == 'NN' and target[i+1][1] == 'VBG':
+						print "  switching", target[i][0], target[i+1][0]
+						target.insert(i,target.pop(i+1))
 
-				# # RULE 3: NN1 of NN2 --> NN2 NN1; from the French "NN1 d'/de NN2", which describes compound nouns in French
-				# # NOTE: This screws up on "passports of states of members of
-				# # the european union", which is a very difficult case due to
-				# # the many "of"s. This is probably worth discussing!
-				# if i < len(target) - 2:
-				# 	if target[i][1][:2] == "NN" and target[i+1][0] == 'of' and target[i+2][1][:2] == "NN":
-				# 		print "  fixing", target[i][0], target[i+1][0], target[i+2][0]
-				# 		target.insert(i, target.pop(i+2))
-				# 		target.pop(i+2)
+				# RULE 3: NN1 of NN2 --> NN2 NN1; from the French "NN1 d'/de NN2", which describes compound nouns in French
+				# NOTE: This screws up on "passports of states of members of
+				# the european union", which is a very difficult case due to
+				# the many "of"s. This is probably worth discussing!
+				# NOTE: ignore if this looks like a list
+				if i < len(target) - 2:
+					if target[i][1][:2] == "NN" and target[i+1][0] == 'of' and target[i+2][1][:2] == "NN":
+						print "  fixing", target[i][0], target[i+1][0], target[i+2][0]
+						target.insert(i, target.pop(i+2))
+						target.pop(i+2)
 
-				# # RULE 4: "HOPE FOR" --> "PLEASE"
-				# if i < len(target) - 1:
-				# 	if target[i][0] == 'hope' and target[i+1][0] == 'for':
-				# 		print "  replacing", target[i][0], target[i+1][0]
-				# 		target.pop(i+1)
-				# 		target[i] = ("please", "NN") # according to NLTK
+				# RULE 4: "HOPE FOR" --> "PLEASE"
+				if i < len(target) - 1:
+					if target[i][0] == 'hope' and target[i+1][0] == 'for':
+						print "  replacing", target[i][0], target[i+1][0]
+						target.pop(i+1)
+						target[i] = ("please", "NN") # according to NLTK
 
-				# # RULE 5: "THE" (cardinal number) (month) --> (cardinal number) (month) 
-				# # SO SOMETHING LIKE "THE 30TH DECEMBER" SHOULD BECOME "30TH DECEMBER ONLY"
-				# if i < len(target) - 2:
-				# 	if target[i][0] == 'the' and isDate(target[i+1][0], target[i+2][0]):
-				# 		print "  removing", target[i][0]
-				# 		target.pop(i)
-				# 		print "  switching", target[i][0], target[i+1][0]
-				# 		target.insert(i,target.pop(i+1))
+				# RULE 5: "THE" (cardinal number) (month) --> (cardinal number) (month) 
+				# SO SOMETHING LIKE "THE 30TH DECEMBER" SHOULD BECOME "30TH DECEMBER ONLY"
+				if i < len(target) - 2:
+					if target[i][0] == 'the' and isDate(target[i+1][0], target[i+2][0]):
+						print "  removing", target[i][0]
+						target.pop(i)
+						print "  switching", target[i][0], target[i+1][0]
+						target.insert(i,target.pop(i+1))
 
-				# # RULE 6: DELETE TWO WORDS IN A ROW THAT ARE THE SAME
-				# # SO "MAIL MAIL" --> "MAIL" OR "BYE BYE" --> "BYE"
-				# if i < len(target) - 1:
-				# 	if target[i][0] == target[i+1][0]:
-				# 		print "  removing", target[i+1][0]
-				# 		target.pop(i+1)
+				# RULE 6: DELETE TWO WORDS IN A ROW THAT ARE THE SAME
+				# SO "MAIL MAIL" --> "MAIL" OR "BYE BYE" --> "BYE"
+				if i < len(target) - 1:
+					if target[i][0] == target[i+1][0]:
+						print "  removing", target[i+1][0]
+						target.pop(i+1)
 
-				# # RULE 7: "NOT" VERB "NO" --> VERB "NO" [DELETE THE "NOT"]
-				# # TRANSLATED FROM THE FRENCH "NE" VERBE "AUCUNE"
-				# # ONLY NEED TO RETAIN THE LAST NO.
-				# # SO "NOT HAS NO" --> "HAS NO"
-				# if i < len(target) - 2:
-				# 	if target[i][0] == 'not' and target[i+1][1][:2] == 'VB' and target[i+2][0] == "no":
-				# 		print "  removing", target[i][0]
-				# 		target.pop(i)
+				# RULE 7: "NOT" VERB "NO" --> VERB "NO" [DELETE THE "NOT"]
+				# TRANSLATED FROM THE FRENCH "NE" VERBE "AUCUNE"
+				# ONLY NEED TO RETAIN THE LAST NO.
+				# SO "NOT HAS NO" --> "HAS NO"
+				if i < len(target) - 2:
+					if target[i][0] == 'not' and target[i+1][1][:2] == 'VB' and target[i+2][0] == "no":
+						print "  removing", target[i][0]
+						target.pop(i)
 
 				# RULE 8: "NOT" VERB "MORE" --> VERB "NO LONGER" 
 				# TRANSLATED FROM THE FRENCH "NE" VERBE "PLUS"
@@ -126,24 +130,24 @@ class DirectTranslation:
 						target[i+1] = ("no", "DT")
 						target.insert(i+2, ("longer", "JJR"))
 
-				# # RULE 9: "OF TO" --> "TO" [DELETE "OF"]
-				# # FRENCH OFTEN REQUIRES PREPS + INFINITIF, NOT NECESSARY
-				# # IN ENGLISH
-				# if i < len(target) - 1:
-				# 	if target[i][0] == "of" and target[i+1][0] == "to":
-				# 		print "  removing", target[i][0]
-				# 		target.pop(i)
+				# RULE 9: "OF TO" --> "TO" [DELETE "OF"]
+				# FRENCH OFTEN REQUIRES PREPS + INFINITIF, NOT NECESSARY
+				# IN ENGLISH
+				if i < len(target) - 1:
+					if target[i][0] == "of" and target[i+1][0] == "to":
+						print "  removing", target[i][0]
+						target.pop(i)
 
-				# # RULE 10: "AT TO LEAVE OF" --> "FROM"
-				# # "A PARTIR DE" --> MORE LIKE "FROM"
-				# if i < len(target) - 3:
-				# 	if target[i][0] == "at" and target[i+1][0] == "to" and target[i+2][0] == "leave" \
-				# 	and target[i+3][0] == "of":
-				# 		for j in range(3):
-				# 			print "  removing", target[i+1][0]
-				# 			target.pop(i+1)
-				# 		print "  replacing", target[i][0]
-				# 		target[i] = ("from", "IN")
+				# RULE 10: "AT TO LEAVE OF" --> "FROM"
+				# "A PARTIR DE" --> MORE LIKE "FROM"
+				if i < len(target) - 3:
+					if target[i][0] == "at" and target[i+1][0] == "to" and target[i+2][0] == "leave" \
+					and target[i+3][0] == "of":
+						for j in range(3):
+							print "  removing", target[i+1][0]
+							target.pop(i+1)
+						print "  replacing", target[i][0]
+						target[i] = ("from", "IN")
 
 
 				###############################################################
@@ -161,7 +165,7 @@ class DirectTranslation:
 
 if __name__ == "__main__":
 	modelname = "model.tmp"
-	if os.path.isfile(modelname):
+	if os.path.isfile(modelname) and False:
 		print "Loading preexisting model..."
 		model = pickle.load(open(modelname, "rb"))
 	else:
