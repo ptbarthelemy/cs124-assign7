add8b619c0fb936494e67b4fb6c85598252f93b4
P. T. Barthelemy <p.t.barthelemy+git@gmail.com>
2013-03-08 11:34:52 -0800

    Completed ten rules. Minor edit to the write-up.

--- code/translate-text.py
+++ code/translate-text.py
@@ -7,6 +7,17 @@ import os.path
 Read the document and translate the text word-by-word.
 """
 
+MONTHS = ['january','february','march','april','may','june','july','august','september','october','november','december']
+
+def isDate(day, month):
+	try:
+		day = int(day) # if day is not an integer value, this will fail
+		assert day <= 31 and day > 0 and month in MONTHS
+		return True
+	except:
+		return False
+
+
 class DirectTranslation:
 	def __init__(self, dictionary):
 		self.loadDictionary(dictionary)
@@ -47,57 +58,104 @@ class DirectTranslation:
 		for source, target in zip(self.source, self.targetPosTags):
 			print "SOURCE  :", ' '.join(source)
 			print "TARGET1 :", ' '.join(list(a for a,b in target))
+			# print "TARGET1 :", target
 			for i in range(len(target)):
 
-				# RULE 1: NN JJ ->> JJ NN
-				if i < len(target) - 1:
-					if target[i][1] == 'NN' and target[i+1][1] == 'JJ':
-						print "  switching", target[i][0], target[i+1][0]
-						temp = target[i]
-						target[i] = target[i+1]
-						target[i+1] = temp
+				# # RULE 1: NN JJ --> JJ NN
+				# if i < len(target) - 1:
+				# 	if target[i][1][:2] == 'NN' and target[i+1][1] == 'JJ':
+				# 		print "  switching", target[i][0], target[i+1][0]
+				# 		target.insert(i,target.pop(i+1))
 
-				# RULE 2: NN VBG ->> VBG NN  
-				if i < len(target) - 1:
-					if target[i][1] == 'NN' and target[i+1][1] == 'VBG':
-						print "  switching", target[i][0], target[i+1][0]
-						temp = target[i]
-						target[i] = target[i+1]
-						target[i+1] = temp
+				# # RULE 2: NN VBG --> VBG NN  
+				# if i < len(target) - 1:
+				# 	if target[i][1][:2] == 'NN' and target[i+1][1] == 'VBG':
+				# 		print "  switching", target[i][0], target[i+1][0]
+				# 		target.insert(i,target.pop(i+1))
 
-				# RULE 3: NN1 of NN2 -> NN2 NN1; from the French "NN1 d'/de NN2", which describes compound nouns in French
+				# # RULE 3: NN1 of NN2 --> NN2 NN1; from the French "NN1 d'/de NN2", which describes compound nouns in French
+				# # NOTE: This screws up on "passports of states of members of
+				# # the european union", which is a very difficult case due to
+				# # the many "of"s. This is probably worth discussing!
+				# if i < len(target) - 2:
+				# 	if target[i][1][:2] == "NN" and target[i+1][0] == 'of' and target[i+2][1][:2] == "NN":
+				# 		print "  fixing", target[i][0], target[i+1][0], target[i+2][0]
+				# 		target.insert(i, target.pop(i+2))
+				# 		target.pop(i+2)
+
+				# # RULE 4: "HOPE FOR" --> "PLEASE"
+				# if i < len(target) - 1:
+				# 	if target[i][0] == 'hope' and target[i+1][0] == 'for':
+				# 		print "  replacing", target[i][0], target[i+1][0]
+				# 		target.pop(i+1)
+				# 		target[i] = ("please", "NN") # according to NLTK
+
+				# # RULE 5: "THE" (cardinal number) (month) --> (cardinal number) (month) 
+				# # SO SOMETHING LIKE "THE 30TH DECEMBER" SHOULD BECOME "30TH DECEMBER ONLY"
+				# if i < len(target) - 2:
+				# 	if target[i][0] == 'the' and isDate(target[i+1][0], target[i+2][0]):
+				# 		print "  removing", target[i][0]
+				# 		target.pop(i)
+				# 		print "  switching", target[i][0], target[i+1][0]
+				# 		target.insert(i,target.pop(i+1))
+
+				# # RULE 6: DELETE TWO WORDS IN A ROW THAT ARE THE SAME
+				# # SO "MAIL MAIL" --> "MAIL" OR "BYE BYE" --> "BYE"
+				# if i < len(target) - 1:
+				# 	if target[i][0] == target[i+1][0]:
+				# 		print "  removing", target[i+1][0]
+				# 		target.pop(i+1)
+
+				# # RULE 7: "NOT" VERB "NO" --> VERB "NO" [DELETE THE "NOT"]
+				# # TRANSLATED FROM THE FRENCH "NE" VERBE "AUCUNE"
+				# # ONLY NEED TO RETAIN THE LAST NO.
+				# # SO "NOT HAS NO" --> "HAS NO"
+				# if i < len(target) - 2:
+				# 	if target[i][0] == 'not' and target[i+1][1][:2] == 'VB' and target[i+2][0] == "no":
+				# 		print "  removing", target[i][0]
+				# 		target.pop(i)
+
+				# RULE 8: "NOT" VERB "MORE" --> VERB "NO LONGER" 
+				# TRANSLATED FROM THE FRENCH "NE" VERBE "PLUS"
+				# THIS TRANSLATES AS "NO LONGER"
 				if i < len(target) - 2:
-					if 'NN' in target[i][1] and target[i+1][0] == 'of' and 'NN' in target[i+2][1]:
-						print "  fixing", target[i][0], target[i+1][0], target[i+2][0]
-						target.pop(i+1)
-						temp = target.pop(i+1)
-						target.insert(i, temp)
+					if target[i][0] == 'not' and target[i+1][1][:2] == 'VB' and target[i+2][0] == "more":
+						print "  removing", target[i][0]
+						target.pop(i)
+						print "  replacing", target[i+1][0]
+						target[i+1] = ("no", "DT")
+						target.insert(i+2, ("longer", "JJR"))
 
-                # RULE 4: "HOPE FOR" ---> "PLEASE"
-                # FOR THOMAS TO FILL IN
+				# # RULE 9: "OF TO" --> "TO" [DELETE "OF"]
+				# # FRENCH OFTEN REQUIRES PREPS + INFINITIF, NOT NECESSARY
+				# # IN ENGLISH
+				# if i < len(target) - 1:
+				# 	if target[i][0] == "of" and target[i+1][0] == "to":
+				# 		print "  removing", target[i][0]
+				# 		target.pop(i)
 
-                # RULE 5: "THE" (cardinal number) (month) -> (cardinal number) (month) 
-                # FOR THOMAS TO FILL IN
-                # SO SOMETHING LIKE "THE 30TH DECEMBER" SHOULD BECOME "30TH DECEMBER ONLY"
+				# # RULE 10: "AT TO LEAVE OF" --> "FROM"
+				# # "A PARTIR DE" --> MORE LIKE "FROM"
+				# if i < len(target) - 3:
+				# 	if target[i][0] == "at" and target[i+1][0] == "to" and target[i+2][0] == "leave" \
+				# 	and target[i+3][0] == "of":
+				# 		for j in range(3):
+				# 			print "  removing", target[i+1][0]
+				# 			target.pop(i+1)
+				# 		print "  replacing", target[i][0]
+				# 		target[i] = ("from", "IN")
 
-                # RULE 6: DELETE TWO WORDS IN A ROW THAT ARE THE SAME
-                # SO "MAIL MAIL" -> "MAIL" OR "BYE BYE" -> "BYE"
 
-                # RULE 7: "NOT" VERB "NO" -> VERB "NO" [DELETE THE "NOT"]
-                # TRANSLATED FROM THE FRENCH "NE" VERBE "AUCUNE"
-                # ONLY NEED TO RETAIN THE LAST NO.
-                # SO "NOT HAS NO" -> "HAS NO"
+				###############################################################
+				# More harm then good
 
-                # RULE 8: "NOT" VERB "MORE" -> VERB "NO LONGER" 
-                # TRANSLATED FROM THE FRENCH "NE" VERBE "PLUS"
-                # THIS TRANSLATES AS "NO LONGER"
+				# # RULE X: IN IN --> IN
+				# # "for of more loose news" --> "for more loose news"
+				# if i < len(target) - 1:
+				# 	if target[i][1] == "IN" and target[i+1][1] == "IN":
+				# 		print "  removing", target[i+1][0]
+				# 		target.pop(i+1)
 
-                # RULE 9: "OF TO" --> "TO" [DELETE "OF"]
-                # FRENCH OFTEN REQUIRES PREPS + INFINITIF, NOT NECESSARY
-                # IN ENGLISH
-
-                # RULE 10: "AT TO LEAVE OF" -> "FROM"
-                # "A PARTIR DE" --> MORE LIKE "FROM"
 
 			print "TARGET2 :", ' '.join(list(a for a,b in target)), "\n"
 
